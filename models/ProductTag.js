2a96ce2d995683bb6c8320468f93e6b3eedcbc25
KIMBERLYSEDDON <82526676+KIMBERLYSEDDON@users.noreply.github.com>
2021-07-12 20:36:06 -0400

    debugged

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -15,10 +15,18 @@ ProductTag.init(
     product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'tag',
-        key: 'id'
+        model: 'product',
+        key: 'id',
       }
     },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      }     
+    },
+
   },
   {
     sequelize,
